4b2941a283dc1310ebbd01916f3ae33e1c57bb9c
seheonCho <whtpgjs94@gmail.com>
2023-10-10 21:57:08 +0900

    feat: 테스트하기 어려운 영역을 분리하기
    - 가게 운영 시간(10:00 ~ 22:00) 외에는 주문을 생성할 수 없는 요구사항
    - 테스트하기 어려운 "시간" 이라는 부분을 외부 계층으로 분리하여 해결

--- src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
+++ src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
@@ -1,7 +1,10 @@
 package sample.cafekiosk.unit;
 
+import java.time.LocalDateTime;
+
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
 
 public class CafeKioskRunner {
 
@@ -15,5 +18,7 @@ public class CafeKioskRunner {
 
 		int totalPrice = cafeKiosk.calculateTotalPrice();
 		System.out.println("총 주문 가격 : " + totalPrice);
+
+		Order order = cafeKiosk.createOrder(LocalDateTime.now());
 	}
 }
