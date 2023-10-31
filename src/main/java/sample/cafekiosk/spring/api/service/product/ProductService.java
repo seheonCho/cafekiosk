c17a2d454dc6d8574c9e82c737e4281678b4fac0
seheonCho <whtpgjs94@gmail.com>
2023-10-31 11:26:24 +0900

    feat: 상품번호 생성 방식 적용
    - Service Layer Transactional 사용
    - 테스트 작성

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -4,18 +4,43 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.dto.request.ProductCreateRequest;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
 	private final ProductRepository productRepository;
+
+	@Transactional
+	public ProductResponse createProduct(ProductCreateRequest request) {
+		String nextProductNumber = createNextProductNumber();
+
+		Product product = request.toEntity(nextProductNumber);
+		Product savedProduct = productRepository.save(product);
+
+		return ProductResponse.of(savedProduct);
+	}
+
+	private String createNextProductNumber() {
+		String latestProductNumber = productRepository.findLatestProductNumber();
+		if (latestProductNumber == null) {
+			return "001";
+		}
+
+		int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+		int nextProductNumberInt = latestProductNumberInt + 1;
+
+		return String.format("%03d", nextProductNumberInt);
+	}
 
 	public List<ProductResponse> getSellingProducts() {
 		List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -23,7 +48,6 @@ public class ProductService {
 		return products.stream()
 			.map(ProductResponse::of)
 			.collect(Collectors.toList());
-
 	}
 
 }
