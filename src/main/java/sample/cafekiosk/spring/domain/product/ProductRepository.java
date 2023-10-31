6ccdb1e945ee4ec953895144a9214f6b2b36e5b0
seheonCho <whtpgjs94@gmail.com>
2023-10-31 11:25:29 +0900

    feat: 상품번호 생성 로직 추가 및 테스트 작성

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.spring.domain.product;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,4 +18,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	List<Product> findAllByProductNumberIn(List<String> productNumbers);
 
+	@Query(value = "select p.product_number from product p order by p.product_number desc limit 1", nativeQuery = true)
+	String findLatestProductNumber();
 }
